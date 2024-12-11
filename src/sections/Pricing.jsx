4930d65d2c2a5ce1db53e1a6e17956a925319227
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-12-11 02:12:51 -0500

    fix

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -10,7 +10,7 @@ const Pricing = () => {
 
     return (
         <section>
-            <Element name="pricing">
+            <Element name="Pricing">
                 <div className="container">
                     <div
                         className="max-w-960 pricing-head_before relative mx-auto border-l border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16">
