de00463656069cf4e2ec42e307b235725c163450
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2024-12-17 03:33:01 -0500

    section Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 export default function App() {
     return (
         <main className={'overflow-hidden'}>
@@ -11,6 +12,7 @@ export default function App() {
             <Features/>
             <Pricing/>
             <Faq/>
+            <Testimonials/>
         </main>
     )
 }
